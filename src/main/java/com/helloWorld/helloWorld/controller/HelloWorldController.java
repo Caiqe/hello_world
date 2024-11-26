51d0bfdfe6d401783536f1af35967424efadb8d8
Caiqe <cttcaiquegomes@gmail.com>
2024-11-26 14:53:37 -0300

    Teste no STS

--- src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
+++ src/main/java/com/helloWorld/helloWorld/controller/HelloWorldController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello-world")
+@RequestMapping
 
 public class HelloWorldController {
 	
-	@GetMapping
+	@GetMapping("/hello-world")
 	public String helloWorld() {
 		return "Hello World!!!";
 	}
